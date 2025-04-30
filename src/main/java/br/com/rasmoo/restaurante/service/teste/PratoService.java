56f75ff9dff61b7e5249cc9813b71e2143276b19
Ruan Lopes <sruan507@gmail.com>
2025-04-30 00:56:38 -0300

    refatoração >> JPAUtil e DAO

--- src/main/java/br/com/rasmoo/restaurante/service/teste/PratoService.java
+++ src/main/java/br/com/rasmoo/restaurante/service/teste/PratoService.java
@@ -1,10 +1,12 @@
 package br.com.rasmoo.restaurante.service.teste;
 
+import br.com.rasmoo.restaurante.dao.PratoDao;
 import br.com.rasmoo.restaurante.entity.Prato;
+import br.com.rasmoo.restaurante.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+// import javax.persistence.EntityManagerFactory;
+// import javax.persistence.Persistence;
 import java.math.BigDecimal;
 
 public class PratoService {
@@ -20,32 +22,28 @@ public class PratoService {
         risoto.setValor(BigDecimal.valueOf(88.50));
         // risoto.setDataDeRegistro(); -> nesse caso não é preciso, já fica automático
 
-        // persistindo informações na tabela
         // EntityManager -> interface JPA que faze o gerenciamento de entidade
         // EntityManagerFactory -> interface JPA que implementa padrão de projeto
 
-        // 2. Criando a fábrica de EntityManager (padrão Factory)
-        // "padrão de projeto" factory - persistindo entidade, passando como parametro persistence.xml >>> <persistence-unit name="rasFood"> - pegamos apenas o name
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("rasFood");
+        // cria gerenciador de entity
+        // Usamos o utilitário que criamos (JPAUtil) para obter um EntityManager.
+        // Agora estamos prontos para conversar com o banco
+        EntityManager entityManager = JPAUtil.getEntityManagerRasFood();
 
-        // 3. Criando o gerenciador de entidades (EntityManager)
-        // criando o EntityManager a partir do Factory
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        // Criamos um DAO para gerenciar a persistência de Prato.
+        // Passamos o EntityManager para ele usar nos métodos (como cadastrar()).
+        PratoDao pratoDao = new PratoDao(entityManager);
 
-        // 4. Iniciando a transação
-        // iniciando transações - caminhando para TRANSIENT
+        // 4. Iniciando a transação - caminhando para TRANSIENT
         entityManager.getTransaction().begin();
 
-        // 5. Salvando (persistindo) o objeto no banco
-        // persistindo a entidade - MANAGED
-        entityManager.persist(risoto);
+        // 5. Salvando (persistindo) o objeto no banco - MANAGED
+        pratoDao.cadastrar(risoto);
 
-        // 6. Confirmando (commit) a transação
-        // sincronizando com o banco de dados - MANAGED
+        // 6. Confirmando (commit) a transação - sincronizando com o banco de dados - MANAGED
         entityManager.getTransaction().commit();
 
-        // 7. Encerrando o gerenciador de entidades
-        // fechando transação - DETACHED
+        // 7. Encerrando o gerenciador de entidades - DETACHED
         entityManager.close();
     }
 }
