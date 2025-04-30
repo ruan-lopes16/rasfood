abfc1e5b64e2e793033cbd7bfaa35b154192c7ae
Ruan Lopes <sruan507@gmail.com>
2025-04-29 21:44:28 -0300

    persistindo primeira entidade

--- src/main/java/br/com/rasmoo/restaurante/service/teste/PratoService.java
+++ src/main/java/br/com/rasmoo/restaurante/service/teste/PratoService.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 public class PratoService {
     public static void main(String[] args) {
 
-        // criando Prato
+        // 1. Criando um novo prato
         Prato risoto = new Prato();
 
         // informações/atributos de risoto
@@ -24,18 +24,27 @@ public class PratoService {
         // EntityManager -> interface JPA que faze o gerenciamento de entidade
         // EntityManagerFactory -> interface JPA que implementa padrão de projeto
 
+        // 2. Criando a fábrica de EntityManager (padrão Factory)
         // "padrão de projeto" factory - persistindo entidade, passando como parametro persistence.xml >>> <persistence-unit name="rasFood"> - pegamos apenas o name
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("rasFood");
 
+        // 3. Criando o gerenciador de entidades (EntityManager)
         // criando o EntityManager a partir do Factory
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+        // 4. Iniciando a transação
         // iniciando transações - caminhando para TRANSIENT
         entityManager.getTransaction().begin();
+
+        // 5. Salvando (persistindo) o objeto no banco
         // persistindo a entidade - MANAGED
         entityManager.persist(risoto);
+
+        // 6. Confirmando (commit) a transação
         // sincronizando com o banco de dados - MANAGED
         entityManager.getTransaction().commit();
+
+        // 7. Encerrando o gerenciador de entidades
         // fechando transação - DETACHED
         entityManager.close();
     }
