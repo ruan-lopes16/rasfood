fc7c7a73cead46bec634bb63d354e87b9e2acecb
Ruan Lopes <sruan507@gmail.com>
2025-04-30 00:55:37 -0300

    classe DAO para acesso ao banco de forma separada

--- src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
+++ src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
@@ -1,4 +1,23 @@
 package br.com.rasmoo.restaurante.dao;
 
+import br.com.rasmoo.restaurante.entity.Prato;
+
+import javax.persistence.EntityManager;
+
 public class PratoDao {
+    // atributo
+    // Cada instância do PratoDao vai usar esse EntityManager para executar operações
+    private EntityManager entityManager;
+
+
+    // Quando você cria um PratoDao, você obriga a passar um EntityManager (injeção manual de dependência)
+    public PratoDao(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    public void cadastrar(Prato prato){
+        this.entityManager.persist(prato);
+        // log
+        System.out.println("Entidade cadastrada: " + prato);
+    }
 }
