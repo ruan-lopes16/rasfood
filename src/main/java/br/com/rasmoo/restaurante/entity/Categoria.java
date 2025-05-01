9e5eefb811100694d86c52275e22e8a6188d6625
Ruan Lopes <sruan507@gmail.com>
2025-05-01 12:28:21 -0300

    entidade categoria concluida

--- src/main/java/br/com/rasmoo/restaurante/entity/Categoria.java
+++ src/main/java/br/com/rasmoo/restaurante/entity/Categoria.java
@@ -9,6 +9,34 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private String nome;
+
+    // construtor vazio
+    public Categoria() {
+    }
+
+    // getters e setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
