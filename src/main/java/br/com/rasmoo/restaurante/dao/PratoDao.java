74d147bc0c4b1442f1c89992a56331f8030cd79f
Ruan Lopes <sruan507@gmail.com>
2025-05-01 10:27:19 -0300

    implementação CRUD ao DAO

--- src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
+++ src/main/java/br/com/rasmoo/restaurante/dao/PratoDao.java
@@ -3,6 +3,7 @@ package br.com.rasmoo.restaurante.dao;
 import br.com.rasmoo.restaurante.entity.Prato;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Id;
 
 public class PratoDao {
     // atributo
@@ -15,9 +16,34 @@ public class PratoDao {
         this.entityManager = entityManager;
     }
 
-    public void cadastrar(Prato prato){
+    /*
+    CRUD
+    C = CREATE -> cadastrar()
+    R = READ -> consultar()
+    U = UPDATE -> atualizar()
+    D = DELETE -> excluir()
+     */
+
+    public void cadastrar(final Prato prato){
         this.entityManager.persist(prato);
         // log
         System.out.println("Entidade cadastrada: " + prato);
+    }
+
+    // buscando por id da entidade
+    public Prato consultar(final Integer id){
+        return this.entityManager.find(Prato.class, id); // procura no banco de dados um objeto da classe Prato com o ID fornecido (entidade + chave primaria)
+    }
+
+    // atualizando entidade (merge -> consigo voltar de detached para managed
+    public void atualizar(final Prato prato){
+        this.entityManager.merge(prato);
+        System.out.println("Entidade atualizada: " + prato);
+    }
+
+    // excluindo entidade
+    public void excluir(final Prato prato){
+        this.entityManager.remove(prato);
+        System.out.println("Entidade removida: " + prato);
     }
 }
